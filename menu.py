871b57ea5218c2e198fbd4ee6b5753184d0a5e24
aaronjul28 <aaronjul28@gmail.com>
2025-03-03 19:36:45 -0600

    Created the function menu_options, right now it is able to fufill point 1 and 2, but it is not in a loop yet

---
+++ menu.py
@@ -0,0 +1,32 @@
+import actions
+
+print("Welcome to the student management tool.")
+
+def menu_options():
+    option=int(input("""
+    1- Add student information
+    2- Review current student information
+    3- Review top 3 students
+    4- Review average grades of each student
+    5- Export data into a CSV
+    6- Import data from a CSV
+    7- Exit
+
+Please select one of the following options: """
+))
+    return option
+
+def option_selected():
+    option=menu_options()
+    if option == int(1):
+        actions.student_data()
+        menu_options()    
+
+    if option == int(2):
+        print("\n************* Students Information *************\n")
+        actions.view_all_students()
+        print("\n************************************************\n")
+        menu_options()
+
+
+print(option_selected())
