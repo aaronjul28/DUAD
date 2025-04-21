99b79e618a4ff5125f82a490900f4336f7cf1870
aaronjul28 <aaronjul28@gmail.com>
2025-04-21 08:39:11 -0600

    • Utilizar un constructor (__init__) para definir el atributo radius. • Usar math.pi en lugar de 3.14 para mayor precisión. • El método get_area() debe ser un método de instancia y retornar el resultado, no solo imprimirlo.

--- semana11/circle.py
+++ semana11/circle.py
@@ -1,10 +1,24 @@
-class Circle:
-    radius=input('Please enter the radius of the circle: ')
-    radius=int(radius)
-    def get_area(radius):
-        area= (radius*radius)*3.14
-        print(f'The area of the circle is {area}')
-    get_area(radius)
+# class Circle:
+#     radius=input('Please enter the radius of the circle: ')
+#     radius=int(radius)
+#     def get_area(radius):
+#         area= (radius*radius)*3.14
+#         print(f'The area of the circle is {area}')
+#     get_area(radius)
     
-Circle()
+# Circle()
+
+import math
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+        self.radius=int(radius)
+
+    def get_area(self):
+        radius=self.radius
+        area= (radius*radius)*math.pi
+        return area
+    
+circle=Circle(input('Enter the radius:'))
+print(circle.get_area())
 
