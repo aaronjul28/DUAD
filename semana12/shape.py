66d89f0a01e9f7eaa35e77b812232351a28c182d
aaronjul28 <aaronjul28@gmail.com>
2025-04-22 22:31:54 -0600

    Perimeter calculator for all shapes is complete, testing done

--- semana12/shape.py
+++ semana12/shape.py
@@ -12,9 +12,23 @@ class Shape(ABC):
     def __init__(self,shape):
         self.shape=shape
 
-    def calculate_perimeter():
-        pass
-            
+    def calculate_perimeter(self):
+        if self.shape=='square':
+            side1=int(input('Enter the size of either of the sides: '))
+            square_perimeter=side1*4
+            return f'The perimeter of the {self.shape} is {square_perimeter}'
+        
+        if self.shape=='rectangle':
+            weight=int(input('Enter the size of the weight: '))
+            height=int(input('Enter the size of the height: '))
+            rectangle_perimeter=(weight*2)+(height*2)
+            return f'The perimeter of the {self.shape} is {rectangle_perimeter}'
+
+        if self.shape=='circle':
+            diametro=int(input('Enter the diameter of the circle: '))
+            circle_perimeter=diametro*math.pi
+            return f'The perimeter of the {self.shape} is {circle_perimeter}'
+
 
     def calculate_area(self):
         #TEST IF THE SHAPE IS BEING LEARNED CORRECTLY
@@ -36,24 +50,46 @@ class Circle(Shape):
         circle_area=self.calculate_area()
         return circle_area
 
+    def perimeter(self):
+        circle_perimeter=self.calculate_perimeter()
+        return circle_perimeter
+
 class Square(Shape):
     def area(self):
         square_area=self.calculate_area()
         return square_area
+
+    def perimeter(self):
+        square_perimeter=self.calculate_perimeter()
+        return square_perimeter
 
 class Rectangle(Shape):
     def area(self):
         rectangle_area=self.calculate_area()
         return rectangle_area
 
+    def perimeter(self):
+        rectangle_perimeter=self.calculate_perimeter()
+        return rectangle_perimeter
+
+
+
+#-------TESTING-------
 #RECTANGLE AREA
-rectangle_area=Rectangle('rectangle')
-print(rectangle_area.area())
+rectangle=Rectangle('rectangle')
+print(rectangle.area())
+print(rectangle.perimeter())
+
+#RECTANGLE PERIMETER
+
+
 
 #SQUARE AREA
-square_area=Square('square')
-print(square_area.area())
+square=Square('square')
+print(square.area())
+print(square.perimeter())
 
 #CIRCLE AREA
-circle_area=Circle('circle')
-print(circle_area.area())
+circle=Circle('circle')
+print(circle.area())
+print(circle.perimeter())
