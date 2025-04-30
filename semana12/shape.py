af9071a6077333d2f33bf1e18f8137026b32d619
aaronjul28 <aaronjul28@gmail.com>
2025-04-29 20:01:24 -0600

    I review all the abstract class again, and apply the concepts correctly this time, testing complete

--- semana12/shape.py
+++ semana12/shape.py
@@ -6,86 +6,55 @@
 from abc import ABC, abstractmethod
 import math
 
-#ABSTRACT PARENT
-class Shape(ABC):
 
-    def __init__(self,shape):
-        self.shape=shape
+class Shape():
+    @abstractmethod
+    def calculate_perimeter():
+        pass
 
-    def calculate_perimeter_square(self,side1):
-        side1=side1
-        square_perimeter=side1*4
-        return f'The perimeter of the {self.shape} is {square_perimeter}'
+    @abstractmethod
+    def calculate_area():
+        pass
 
-    def calculate_perimeter_rectangle(self,weight,height):
-            weight=weight
-            height=height
-            rectangle_perimeter=(weight*2)+(height*2)
-            return f'The perimeter of the {self.shape} is {rectangle_perimeter}'
-
-    def calculate_perimeter_circle(self,diameter):
-        diameter=diameter
-        circle_perimeter=diameter*math.pi
-        return f'The perimeter of the {self.shape} is {circle_perimeter}'
-
-
-    def calculate_area_rectangle_square(self,lenght,width):
-        lenght=lenght
-        width=width
-        area=lenght*width
-        return f'The area of the {self.shape} is {area}'
-    
-    def calculate_area_circle(self,radius):
-        radius=radius
-        area=(radius*radius)*math.pi
-        return f'The area of the {self.shape} is {area}'
-        
-#INHERIT SHAPE
-@abstractmethod
 class Circle(Shape):
-    def area(self):
-        circle_area=self.calculate_area_circle()
-        return circle_area
+    def calculate_perimeter(self,diameter):
+        perimeter=diameter*math.pi
+        return f'The perimeter of the circle is {perimeter}'
 
-    def perimeter(self):
-        circle_perimeter=self.calculate_perimeter_circle()
-        return circle_perimeter
-@abstractmethod
+    def calculate_area(self,radius):
+        area=(radius*radius)*math.pi
+        return f'The area of the circle is {area}'
+
 class Square(Shape):
-    def area(self):
-        square_area=self.calculate_area_rectangle_square()
-        return square_area
+    def calculate_perimeter(self,side):
+        perimeter=side*4
+        return f'The perimeter of the square is {perimeter}'
 
-    def perimeter(self):
-        square_perimeter=self.calculate_perimeter_square()
-        return square_perimeter
+    def calculate_area(self,lenght,width):
+        area=lenght*width
+        return f'The area of the square is {area}'
 
-@abstractmethod
 class Rectangle(Shape):
-    def area(self):
-        rectangle_area=self.calculate_area_rectangle_square()
-        return rectangle_area
+    def calculate_perimeter(self,lenght,width):
+        perimeter=(lenght*2)+(width*2)
+        return f'The perimeter of the rectangle is {perimeter}'
 
-    def perimeter(self):
-        rectangle_perimeter=self.calculate_perimeter_rectangle()
-        return rectangle_perimeter
-
-
-
-#-------TESTING-------
-#RECTANGLE 
-rectangle=Rectangle('rectangle')
-print(rectangle.calculate_perimeter_rectangle(5,5))
-print(rectangle.calculate_area_rectangle_square(5,5))
+    def calculate_area(self,lenght,width):
+        area=lenght*width
+        return f'The area of the rectangle is {area}'
 
 
-#SQUARE 
-square=Square('square')
-print(square.calculate_perimeter_square(5))
-print(square.calculate_area_rectangle_square(5,5))
+circle_perimeter=Circle()
+print(circle_perimeter.calculate_perimeter(5))
+print(circle_perimeter.calculate_area(5))
 
-#CIRCLE
-circle=Circle('circle')
-print(circle.calculate_perimeter_circle(5))
-print(circle.calculate_area_circle(5))
+square_perimeter=Square()
+print(square_perimeter.calculate_perimeter(5))
+print(square_perimeter.calculate_area(5,5))
+
+
+square_perimeter=Rectangle()
+print(square_perimeter.calculate_perimeter(5,5))
+print(square_perimeter.calculate_area(5,5))
+
 
